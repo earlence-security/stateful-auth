535044020f894e75797abdbe957e52a44bbbe81b
Zhongrui-Cao <z2cao@ucsd.edu>
2023-08-14 00:02:20 -0700

    Connect Client with Auth server

---
+++ Client/app.py
@@ -0,0 +1,49 @@
+from flask import Flask, url_for, session
+from flask import render_template, redirect
+from authlib.integrations.flask_client import OAuth
+
+
+app = Flask(__name__)
+app.secret_key = 'secret'
+app.config.from_object('config')
+
+oauth = OAuth(app)
+oauth.register(
+    name='testClient',
+    client_id=app.config['CLIENT_ID'],
+    client_secret=app.config['CLIENT_SECRET'],
+    access_token_url='http://127.0.0.1:5000/oauth/token',
+    access_token_params=None,
+    authorize_url='http://127.0.0.1:5000/oauth/authorize',
+    authorize_params=None,
+    client_kwargs={
+        'scope': 'profile'
+    }
+)
+
+
+@app.route('/')
+def homepage():
+    token = session.get('token')
+    return render_template('home.html', token=token)
+
+
+@app.route('/login')
+def login():
+    redirect_uri = url_for('auth', _external=True)
+    return oauth.testClient.authorize_redirect(redirect_uri)
+
+
+@app.route('/auth')
+def auth():
+    token = oauth.testClient.authorize_access_token()
+    #TODO
+    #Put in database not session
+    session['token'] = token
+    return redirect('/')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('token', None)
+    return redirect('/')
