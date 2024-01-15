61e0866cfc8cf085d9839514280d8c827ad78cda
Zhongrui-Cao <z2cao@ucsd.edu>
2024-01-14 23:04:43 -0800

    add REDIRECT_URI in config

--- Client/config.py
+++ Client/config.py
@@ -7,6 +7,7 @@ CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 ACCESS_TOKEN_URL = os.getenv('ACCESS_TOKEN_URL')
 AUTHORIZE_URL = os.getenv('AUTHORIZE_URL')
+REDIRECT_URI = os.getenv('REDIRECT_URI')
 CLIENT_KWARGS = {
     'scope': 'profile',
     'policy_hashes': read_policy_hashes()
