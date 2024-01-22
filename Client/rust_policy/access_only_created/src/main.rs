bd2e991e12663f2062d0f05dd918a80fa6803fe7
Luoxi Meng <luoxim@umich.edu>
2024-01-21 20:37:39 -0800

    Add access-only-created policy

---
+++ Client/rust_policy/access_only_created/src/main.rs
@@ -0,0 +1,67 @@
+//use serde_json::{Result, Value};
+use std::env;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+#[derive(Serialize, Deserialize)]
+struct Request {
+    method: String,
+    uri: String,
+    path: String,
+    body: String,
+    headers: HashMap<String, String>,
+    time: f32
+}
+
+#[derive(Deserialize, Serialize)]
+struct History {
+    api: String,
+    method: String,
+    counter: i32,
+    timestamp: f64,
+}
+
+type HistoryMap = HashMap<String, Vec<History>>;
+
+//TODO: add error handling
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let json_str = &args[1];
+    let hist_str = &args[2];
+
+    let parsed_req: Request = read_json(json_str);
+    let parsed_hist: HistoryMap = read_history(hist_str);
+
+    // Check history if accessing an existing event.
+    if parsed_req.path.starts_with("/api/events") {
+        // Accept if creating a new event.
+        if parsed_req.path == "/api/events" {
+            println!("Accept");
+            return
+        }
+        // Check history if modifying or delete an existing event.
+        for (_obj_id, history) in parsed_hist {
+            for entry in history {
+                if entry.method == "POST" && entry.api == "/api/events" {
+                    println!("Accept");
+                    return
+                }
+            }
+        }
+        println!("Deny");
+    } else {
+        println!("Accept");
+    }
+}
+
+fn read_json(json_str: &str) -> Request {
+    // Parse the string of data into serde_json::Value.
+    let v: Request = serde_json::from_str(json_str).unwrap();
+    return v;
+}
+
+fn read_history(json_str: &str) -> HistoryMap {
+    // Parse the string of data into serde_json::Value.
+    let v: HistoryMap = serde_json::from_str(json_str).unwrap();
+    return v;
+}
