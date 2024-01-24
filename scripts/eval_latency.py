654caae6e55ea9d09566118f656b47bb1cb6275d
Luoxi Meng <luoxim@umich.edu>
2024-01-24 12:15:32 -0800

    Update latency eval script

--- scripts/eval_latency.py
+++ scripts/eval_latency.py
@@ -79,10 +79,10 @@ fake_history_entries = [
 apis = [(e["api"], e["method"])  for e in fake_history_entries]
 apis_ids = [(e["api"], e["method"]) for e in fake_history_entries if "<event_id>" not in e["api"]]
 
-def generate_requests(max_objects, n_iters, deny_ratio=0.2):
+def generate_requests(max_objects, num_objects_step, n_iters, deny_ratio=0.2):
     reqs = []
     # Option 1: mix the endpoints
-    for i in range(1, max_objects+1):
+    for i in range(5, max_objects+1, num_objects_step):
         for _ in range(n_iters):
             path, method = random.choice(apis_ids)
             ids = [str(uuid4()) for _ in range(i)]
@@ -96,7 +96,7 @@ def generate_requests(max_objects, n_iters, deny_ratio=0.2):
                 for e in history:
                     e["api"] = e["api"].replace('<event_id>', id)
                 batch_history[id] = history
-            reqs.append((path, method, json.dumps(data), batch_history))
+            reqs.append((i, path, method, json.dumps(data), batch_history))
 
     # Option 2: measure the latency of every single endpoint
     # ids = []
@@ -127,7 +127,7 @@ def generate_requests(max_objects, n_iters, deny_ratio=0.2):
 
 def measure_latency(base_url, token, reqs):
     latency = []
-    for path, method, data, batch_history in reqs:
+    for i, path, method, data, batch_history in reqs:
         headers = {
             'Authorization': f'Bearer {token}',
             'Authorization-History': json.dumps(batch_history),
@@ -146,12 +146,13 @@ def main():
     parser.add_argument('--token', type=str, default='token')
     parser.add_argument('--base-url', type=str, default='http://127.0.0.1:5000/api')
     # parser.add_argument('--num-requests', type=int, default=300)
-    parser.add_argument('--n_iters', type=int, default=30)
+    parser.add_argument('--n-iters', type=int, default=30)
     parser.add_argument('--max-objects', type=int, default=10)
+    parser.add_argument('--step', type=int, default=1)
     parser.add_argument('--deny-ratio', type=float, default='0.2')
     args = parser.parse_args()
     
-    reqs = generate_requests(args.max_objects, args.n_iters, args.deny_ratio)
+    reqs = generate_requests(args.max_objects, args.step, args.n_iters, args.deny_ratio)
     with open('reqs.json', 'w') as f:
         json.dump(reqs, f, indent=4)
 
@@ -161,9 +162,9 @@ def main():
     latency = measure_latency(args.base_url, args.token, reqs)
 
     with open('latency.csv', 'w') as f:
-        f.write('method,path,latency\n')
-        for (path, method, _, _), l in zip(reqs, latency):
-            f.write(f'{method},{path},{l}\n')
+        f.write('num_objects, method,path,latency\n')
+        for (i, path, method, _, _), l in zip(reqs, latency):
+            f.write(f'{i},{method},{path},{l}\n')
 
 
 if __name__ == '__main__':
